dde5258057bed225145ae7ef25eb4f9c6dcfa982
kai618 <vanhieu1601@gmail.com>
2020-04-26 18:38:45 +0700

    able to fetch product types

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using thewayshop.Models;
+
+namespace thewayshop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly eshopContext _ctx = new eshopContext();
+
+        public ActionResult Index()
+        {
+            ViewBag.types = _ctx.LoaiSanPhams.Take(6).ToList();
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="thewayshop.MvcApplication" Language="C#" %>
