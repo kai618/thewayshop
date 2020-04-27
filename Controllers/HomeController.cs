d4d466bad58155a224f1ef9d9b51b6a3c4e5acda
kai618 <vanhieu1601@gmail.com>
2020-04-27 11:49:29 +0700

    added more images

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using thewayshop.Models;
 
@@ -11,6 +12,11 @@ namespace thewayshop.Controllers
         public ActionResult Index()
         {
             ViewBag.types = _ctx.LoaiSanPhams.Take(6).ToList();
+
+            ViewBag.newProducts =
+                _ctx.SanPhams.Where(p => p.NgayThemSP > DateTime.Today.AddMonths(-2)).Take(4).ToList();
+
+            ViewBag.saleProducts = _ctx.SanPhams.Where(p => p.KhuyenMai > 0).Take(4).ToList();
 
             return View();
         }
