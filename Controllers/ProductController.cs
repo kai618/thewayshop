1f32111ceaa6607dc026b0ccafe6e7d4f16244f0
kai618 <vanhieu1601@gmail.com>
2020-05-08 07:37:34 +0700

    able to show products by typeId in productPage

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -3,17 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using thewayshop.Models;
 
 namespace thewayshop.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly eshopEntities _ctx = new eshopEntities();
+
         // GET: Shop
         public ActionResult Filter(string typeId)
         {
             ViewBag.selectedType = typeId;
+            ViewBag.products = _ctx.SanPhams.Where(s => s.MaLoaiSP == typeId).ToList();
 
             return View();
+        }
+
+        public ActionResult Search(string keyword)
+        {
+            ViewBag.products = _ctx.SanPhams.Where(s => s.TenSP.Contains(keyword));
+
+            return View("Filter");
         }
     }
 }
--- theway/js/custom.js
+++ theway/js/custom.js
@@ -16,13 +16,13 @@
 	   Fixed Menu
 	   ................................................. */
 
-	$(window).on('scroll', function() {
-		if ($(window).scrollTop() > 50) {
-			$('.main-header').addClass('fixed-menu');
-		} else {
-			$('.main-header').removeClass('fixed-menu');
-		}
-	});
+	//$(window).on('scroll', function() {
+	//	if ($(window).scrollTop() > 100) {
+	//		$('.main-header').addClass('fixed-menu');
+	//	} else {
+	//		$('.main-header').removeClass('fixed-menu');
+	//	}
+	//});
 
 	/* ..............................................
 	   Gallery
