d786d8f19e64f5c0f9e6f950438480f8021c0138
Hieu Pham <vanhieu1601@gmail.com>
2020-06-01 17:26:50 +0700

    add product list functions

--- theway/js/side-cart.js
+++ theway/js/side-cart.js
@@ -1,9 +1,8 @@
 ﻿const productListKey = 'thewayshop-products';
+const productListEl = document.getElementById('product-list');
 const productList = [];
 
-fetchProductList();
-
-function addProduct(id, name, price, amount) {
+function addProduct(id, name, price, amount, imageURL) {
   const index = productList.findIndex(p => p.id === id);
   if (index === -1)
     productList.push({
@@ -11,10 +10,55 @@ function addProduct(id, name, price, amount) {
       name: name,
       price: price,
       amount: amount,
+      imageURL: imageURL
     })
   else productList[index].amount++;
 
   storeProductList();
+  refreshProductList();
+}
+
+function removeProduct(id) {
+  // 
+  setSum();
+}
+
+function refreshProductList() {  
+  if (productList.length) {
+    let rows = '';
+    for (let p of productList) {
+      rows += `
+        <li>
+          <a href="#" class="photo">
+            <img src="${p.imageURL}" class="cart-thumb" alt="" />
+          </a>
+          <h6>
+            <a href="#">${p.name}</a>
+          </h6>
+          <p>${p.amount}x - <span class="price">$${p.price}</span></p>
+        </li>
+      `;
+    }
+
+    rows += `
+      <li class="total">
+        <a href="#" class="btn btn-default hvr-hover btn-cart">VIEW CART</a>
+        <span class="float-right"><strong>Total</strong>: $${getSum()}</span>
+      </li>
+    `;
+
+    productListEl.innerHTML = rows;
+  } else {
+    productListEl.innerHTML = `
+      <p>Empty Cart</p>
+    `;
+  }
+}
+
+function getSum() {
+  let sum = 0;
+  productList.forEach(p => sum += p.price * p.amount);
+  return sum;
 }
 
 function fetchProductList() {
@@ -22,7 +66,7 @@ function fetchProductList() {
   if (!jsonData) return [];
 
   productList.push(...JSON.parse(jsonData));
-  console.log(productList);
+  refreshProductList();
 }
 
 function storeProductList() {
@@ -30,9 +74,4 @@ function storeProductList() {
   localStorage.setItem(productListKey, JSON.stringify(productList));
 }
 
-const cartItemModel = {
-  id: 1,
-  name: 'Apple',
-  price: 10,
-  amount: 5,
-}
+fetchProductList();
